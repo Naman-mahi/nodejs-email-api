3812d08f7245c603311f1f647fe1dbf3b73ccd6f
Sunil Khobragade <sunil@intencode.com>
2024-05-16 11:33:24 +0530

    Initial commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
